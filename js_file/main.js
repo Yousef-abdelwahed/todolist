d0baeff1d03350e54f287461b77c2aa8b58f78bd
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2022-11-08 01:57:42 +0200

    IS Exist USer
    check is Exist function

--- js_file/main.js
+++ js_file/main.js
@@ -1,55 +1,72 @@
+// issues
+//all data of array is played #done 
+//the data stored just single array #done
+
 let form=document.getElementById("form");
 // let searchUser =document.getElementById("searchUser");
 
 let users= [];
-let tasks=[] 
+let tasks=[]; 
+var arr=[];
 
+///***************is Exists*******************
+function isExist(){
+    for (let i=0;i<users.length;i++){
+        if(users[i].name===document.getElementById("userName").value){
+            alert("T")
+            return true;
+        }else{
+            alert("F")
+
+           return false;
+        }
+    }
+}
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     isExist();
-    login();
-    display();
-   console.log(isExist())
-    })
-    
-    function login(){
-        let userEmail =document.getElementById("email").value;
-        let userPsw = document.getElementById("psw").value;
-        let userName= document.getElementById("userName").value
-    let user={
-        name:userName,
-        email:userEmail,
-        password:userPsw
-    };
-    
-    //store user data 
-    users=JSON.parse((localStorage.getItem("user")))||[];
-    users.push(user);
-   localStorage.setItem("users",JSON.stringify(users));
-   
-   return true;
-}
-function display(){
-    for(let i=0; i<users.length;i++){
-        let li=document.createElement("li");
-        let listUsers  =document.getElementById("listUer");
-        li.innerHTML=users[i];
-        listUsers.appendChild(li); 
-        listUsers.style.backgroundColor="green";         
-    }};
-
-//is Exists
-    function isExist(){
-        for (let i=0;i<users.length;i++){
-            if(users[i].name==userName){
-                console.log(userName)
-                return true;
-            }else{
-               return false
-            }
-        }
+    console.log(isExist())
+    if(isExist()==false || isExist()!= "undefined"){
+        login();
+        display();
+    }else{
+        alert("a")
     }
+    
+  
+     
+       });
+       
+       function login(){
+           const userName= document.getElementById("userName").value,
+           userEmail =document.getElementById("email").value,
+           userPsw = document.getElementById("psw").value;
+           let user={
+               name:userName,
+                email:userEmail,
+                password:userPsw
+    };
+    //store user data 
+    users=JSON.parse(localStorage.getItem("user"))||[];
+    users.push(user)
+   localStorage.setItem("user",JSON.stringify(users));
+}
+
+function display(){
+  
+   for(let i=0; i<users.length ; i++){
+    arr[i]=users[i]
+};
+    for(let i=0; i<arr.length;i++){
+        let li=document.createElement("li");
+        let listUsers=document.getElementById("listUer");
+        li.innerHTML=arr[arr.length-1].name;
+        console.log(users)
+        listUsers.style.backgroundColor="green";
+        listUsers.appendChild(li);
+             break;}};
+
 
 
 
@@ -63,38 +80,36 @@ function display(){
 // }
 
 //****************Create List of Tasks ****************/
-function btnList(){
-    getList();
-    setList();
-}
+// function btnList(){
+//     getList();
+//     setList();
+// }
 
-function getList(){
-    let getTask=document.getElementById("getTask").value;
-    let task={
-        userTask:getTask
-    }
-    tasks.push(task);
-}
-function setList(){
-    var container="";
+// function getList(){
+//     let getTask=document.getElementById("getTask").value;
+//     let task={
+//         userTask:getTask
+//     }
+//     tasks.push(task);
+// }
+// function setList(){
+//     var container="";
     
-        for(let i=0; i<tasks.length;i++){
-            if (tasks[i].userTask =""){
-               alert("fill in your Tasks ")
-            }else{
-                container+=`
-                <tr>
-                <td>${tasks[i].userTask} </td>
-                <td> <button id="taskBtn" onclick="deletTask(${i})" class="btn btn-danger">Delet</button>
-                </td>
-                </tr>  `;
-            }
-           
-          
-document.getElementById("setTask").innerHTML=container;
-}};
+//         for(let i=0; i<tasks.length;i++){
+//             if (tasks[i].userTask =""){
+//                alert("fill in your Tasks ")
+//             }else{
+//                 container+=`
+//                 <tr>
+//                 <td>${tasks[i].userTask} </td>
+//                 <td> <button id="taskBtn" onclick="deletTask(${i})" class="btn btn-danger">Delet</button>
+//                 </td>
+//                 </tr>  `;
+//             };         
+// document.getElementById("setTask").innerHTML=container;
+// }};
 //*****Delet Task******* */
-function deletTask(index){
-   tasks.splice(index,1);
-   setList();
-};
+// function deletTask(index){
+//    tasks.splice(index,1);
+//    setList();
+// };
